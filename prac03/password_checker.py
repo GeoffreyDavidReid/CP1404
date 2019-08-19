fbcc00f26e3627419fdb50b1f2989f0425834f42
gdreid2015 <gdreid2015@hotmail.com>
2019-08-20 09:56:54 +1000

    Refactor password program to use functions

--- prac03/password_checker.py
+++ prac03/password_checker.py
@@ -1,14 +1,20 @@
+#
 # Password Checker
 #
-minimum_length = 10
-password = input("Enter password: ")
-password_ok = True
-while password_ok == True:
-    if len(password) > minimum_length:
-        break
-    password = input("Enter password: ")
-print("Password is: ", password)
-print("Password length: ", len(password))
 
-for char in password:
-    print("*")
+def main():
+
+    minimum_length = 10
+    password = input("Enter password: ")
+    password_ok = True
+    while password_ok == True:
+        if len(password) > minimum_length:
+            break
+        password = input("Enter password: ")
+    print("Password is: ", password)
+    print("Password length: ", len(password))
+
+    for char in password:
+        print("*")
+
+main()
