626c866b6d1caa2577488d0e14a4566a4fc5b9a0
gdreid2015 <gdreid2015@hotmail.com>
2019-08-20 09:34:43 +1000

    Initial commit for password_checker

---
+++ prac03/password_checker.py
@@ -0,0 +1,14 @@
+# Password Checker
+#
+minimum_length = 10
+password = input("Enter password: ")
+password_ok = True
+while password_ok == True:
+    if len(password) > minimum_length:
+        break
+    password = input("Enter password: ")
+print("Password is: ", password)
+print("Password length: ", len(password))
+
+for char in password:
+    print("*")
