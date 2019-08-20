e46e1a1d011e6415173dfcd098ced33b9293c217
gdreid2015 <gdreid2015@hotmail.com>
2019-08-20 11:20:39 +1000

    Remove unnecessary print statements

--- prac03/temperatures.py
+++ prac03/temperatures.py
@@ -1,19 +1,18 @@
 """
 CP1404/CP5632 - Practical
 Program for temperature conversion
+
 """
 
 def celsius_to_fahrenheit(celcius):
     #convert celcius to fahrenheit
-    celsius = float(input("Celsius: "))
+    #celsius = float(input("Celsius: "))
     fahrenheit = celsius * 9.0 / 5 + 32
-    print("Result: {:.2f} F".format(fahrenheit))
     return fahrenheit
 
 def fahrenheit_to_celsius(fahrenheit):
     #convert fahrenheit to celsius
     celsius = 5 / 9 * (fahrenheit - 32)
-    print("Result: {:.2f} C".format(celsius))
     return celsius
 
 MENU = """C - Convert Celsius to Fahrenheit
@@ -25,16 +24,13 @@ while choice != "Q":
     if choice == "C":
         celsius = float(input("Celsius: "))
         fahrenheit = celsius_to_fahrenheit(celsius)
-        #fahrenheit = celsius * 9.0 / 5 + 32
-        #print("Result: {:.2f} F".format(fahrenheit))
+        print("Result: {:.2f} F".format(fahrenheit))
     elif choice == "F":
         fahrenheit = float(input("Fahrenheit: "))
         celsius = fahrenheit_to_celsius(fahrenheit)
-        #celsius = 5 / 9 * (fahrenheit - 32)
-        #print("Result: {:.2f} C".format(celsius))
+        print("Result: {:.2f} C".format(celsius))
     else:
         print("Invalid option")
-        break
     print(MENU)
     choice = input(">>> ").upper()
 print("Thank you.")
