ed1318d18d01592c5cd5a6fb5b8b0ca46dd074ca
gdreid2015 <gdreid2015@hotmail.com>
2019-08-25 09:17:35 +1000

    list_warmup code and output

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -3,12 +3,25 @@
 
 numbers = [3, 1, 4, 1, 5, 9, 2]
 
-numbers[0]
-numbers[-1]
-numbers[3]
-numbers[:-1]
-numbers[3,4]
-5 in numbers
-7 in numbers
-"3"in numbers
-numbers + [6, 5, 3]
+print(numbers[0])
+print(numbers[-1])
+print(numbers[3])
+print(numbers[:-1])
+print(numbers[3:4])
+print(5 in numbers)
+print(7 in numbers)
+print("3"in numbers)
+print(numbers + [6, 5, 3])
+
+#output of list_warmup
+
+#3
+#2
+#1
+#[3, 1, 4, 1, 5, 9]
+#[1]
+#True
+#False
+#False
+#[3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
