492abb53e7cab980a708f077f8f224a05871082b
gdreid2015 <gdreid2015@hotmail.com>
2019-08-25 15:46:47 +1000

    Start Quick picks main function

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -8,3 +8,9 @@ NUMBERS_PER_LINE = 6
 MINIMUM = 1
 Maximum = 45
 
+def main():
+    """Quick picks program - choose sets of random numbers."""
+    number_of_quick_picks = int(input("How many quick picks? "))
+    while number_of_quick_picks < 0:
+        print("That makes no sense!")
+        number_of_quick_picks = int(input("How many quick picks? "))
