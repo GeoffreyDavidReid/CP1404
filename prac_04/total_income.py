453da79ac7eab914eb13c9e840fa7af783f84181
gdreid2015 <gdreid2015@hotmail.com>
2019-08-25 12:50:11 +1000

    Add Income Report function

--- prac_04/total_income.py
+++ prac_04/total_income.py
@@ -4,6 +4,16 @@ Starter code for cumulative total income program
 """
 
 
+def print_report(month, months_int, incomes):
+    total = 0
+    print()
+    print("Income Report")
+    print("-------------")
+    for month in range(1, months_int + 1):
+        income = incomes[month - 1]
+        total += income
+        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+
 def main():
     """Display income report for incomes over a given number of months."""
     incomes = []
@@ -12,11 +22,6 @@ def main():
     for month in range(1, months_int + 1):
         income = float(input('{} {} {}'.format("Enter income for month ", str(month), ": ")))
         incomes.append(income)
-
-    total = 0
-    for month in range(1, months_int + 1):
-        income = incomes[month - 1]
-        total += income
-        print("Month {:2} - Income: ${:10.2f} Total: ${:10.2f}".format(month, income, total))
+    print_report(month, months_int, incomes)
 
 main()
