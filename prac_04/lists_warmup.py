d2e3bce43a4bad99f65b705a2e96f28495687765
gdreid2015 <gdreid2015@hotmail.com>
2019-08-25 13:24:51 +1000

    Initialise

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -25,3 +25,21 @@ print(numbers + [6, 5, 3])
 #False
 #[3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
 
+#1. Change the first element of numbers to "ten"
+
+numbers[0] = "ten"
+print(numbers)
+
+#2. Change the last element of numbers 1
+
+numbers[-1] = "8"
+print(numbers)
+
+#3. Get all the elements from numbers except the first two
+
+#numbers = numbers[2:]
+print(numbers[2:])
+
+#4. Check if 9 is an element of numbers
+
+print(9 in numbers)
