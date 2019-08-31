772f05a27396d3c6e4761c0a656c2354dff7348c
gdreid2015 <gdreid2015@hotmail.com>
2019-08-31 19:40:11 +1000

    Change header comment

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,7 +1,6 @@
 """
 CP1404/CP5632 Practical
-State names in a dictionary
-File needs reformatting
+Colour names in a dictionary
 """
 
 COLOUR_NAMES = dict(AliceBlue="#f0f8ff", AntiqueWhite="#faebd7", AntiqueWhite1="#ffefdb  \
