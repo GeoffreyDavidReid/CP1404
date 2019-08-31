96b71255fdd2d0fa8344ab3426a4322c7c83740b
gdreid2015 <gdreid2015@hotmail.com>
2019-08-31 21:12:20 +1000

    Refactor with for loop to create dictionary word_count_dict

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,1 +1,14 @@
 """ Word Occurrences """
+
+words_str = str(input("Enter string of words: "))
+words_list = words_str.split()
+
+word_count_dict = {}
+
+for word in words_list:
+    if word in word_count_dict:
+        word_count_dict[word] += 1
+    else:
+        word_count_dict[word] = 1
+
+print(word_count_dict)
