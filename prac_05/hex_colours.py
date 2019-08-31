cf396eabed8acaaf9f09b771ad6bf2b637b46efe
gdreid2015 <gdreid2015@hotmail.com>
2019-08-31 20:05:09 +1000

    Remove print colour_names test code after dictionary constant

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -7,7 +7,6 @@ COLOUR_NAMES = dict(AliceBlue="#f0f8ff", AntiqueWhite="#faebd7", AntiqueWhite1="
                 ", AntiqueWhite2="#eedfcc", AntiqueWhite3="#cdc0b0", AntiqueWhite4="#8b8378  \
                 ", aquamarine1="#7fffd4", aquamarine2="#76eec6", aquamarine4="#458b74  \
                 ", azure1="#f0ffff" , azure2="#e0eeee")
-#print(COLOUR_NAMES)
 
 colour_name = input("Enter colour name ie AliceBlue: ")
 while colour_name != "":
