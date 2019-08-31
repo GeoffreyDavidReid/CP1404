a99e70a312dd3c0cb833a5d75494091246c864a8
gdreid2015 <gdreid2015@hotmail.com>
2019-08-31 19:38:37 +1000

    Create COLOUR_NAMES constant with dict constructor

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -4,7 +4,10 @@ State names in a dictionary
 File needs reformatting
 """
 
-COLOUR_NAMES = {"AliceBlue": "#f0f8ff", "AntiqueWhite": "#faebd7", "AntiqueWhite1": "#ffefdb"}
+COLOUR_NAMES = dict(AliceBlue="#f0f8ff", AntiqueWhite="#faebd7", AntiqueWhite1="#ffefdb  \
+                ", AntiqueWhite2="#eedfcc", AntiqueWhite3="#cdc0b0", AntiqueWhite4="#8b8378  \
+                ", aquamarine1="#7fffd4", aquamarine2="#76eec6", aquamarine4="#458b74  \
+                ", azure1="#f0ffff" , azure2="#e0eeee")
 #print(COLOUR_NAMES)
 
 colour_name = input("Enter colour name ie AliceBlue: ")
