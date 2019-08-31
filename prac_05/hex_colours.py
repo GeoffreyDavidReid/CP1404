2cacd0702c86f0fb93a69ec8e9debd0f52cecb6f
gdreid2015 <gdreid2015@hotmail.com>
2019-08-31 20:03:39 +1000

    Remove colour_name upper method as it doesn't work for colour_name's of form AliceBlue which is neither all upper or all lower

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -10,14 +10,13 @@ COLOUR_NAMES = dict(AliceBlue="#f0f8ff", AntiqueWhite="#faebd7", AntiqueWhite1="
 #print(COLOUR_NAMES)
 
 colour_name = input("Enter colour name ie AliceBlue: ")
-#colour_name = str.upper(colour_name)
 while colour_name != "":
     if colour_name in COLOUR_NAMES:
         print("{} is {}".format(colour_name, COLOUR_NAMES[colour_name]))
     else:
         print("Invalid colour name")
     colour_name = input("Enter colour name ie AliceBlue: ")
-    #colour_name = str.upper(colour_name)
+
 print()
 for colour_name in COLOUR_NAMES:
     print("{} is {}".format(colour_name, COLOUR_NAMES[colour_name]))
