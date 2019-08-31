4fd6c6684b8864c5c611d062a060fb98659b0b14
gdreid2015 <gdreid2015@hotmail.com>
2019-08-31 15:37:49 +1000

    Reformat formatting of the dictionary constant ALL_CAPS

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -5,7 +5,7 @@ File needs reformatting
 """
 
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
-STATE_NAMES = {"QLD":"Queensland", "NSW": "New South Wales", "NT" :"Northern Territory", "WA" : "Western Australia", "ACT":"Australian Capital Territory", "VIC":"Victoria", "TAS":"Tasmania"}
+STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia", "ACT": "Australian Capital Territory", "VIC":"Victoria", "TAS": "Tasmania"}
 # print(STATE_NAMES)
 
 
@@ -16,4 +16,3 @@ while state != "":
     else:
         print("Invalid short state")
     state = input("Enter short state: ")
-    
