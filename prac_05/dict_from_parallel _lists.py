4e6c0f84f1b69fde9bb28395d7c75df5e4e30899
gdreid2015 <gdreid2015@hotmail.com>
2019-09-01 16:52:59 +1000

    Print lists out for testing

--- prac_05/dict_from_parallel _lists.py
+++ prac_05/dict_from_parallel _lists.py
@@ -4,3 +4,12 @@ first_list = ("a", "b", "c", "d")
 second_list = (1, 2, 3, 4)
 
 def dict_from_lists(first_list, second_list):
+    for index in first_list:
+        print(index, end=" ")
+
+    print("\n")
+    
+    for index in second_list:
+        print(index, end=" ")
+
+dict_from_lists(first_list, second_list)
