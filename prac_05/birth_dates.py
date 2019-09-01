f73ba4cd9eb30c6b8f67968e389a1e9bfbd7c20a
gdreid2015 <gdreid2015@hotmail.com>
2019-09-01 15:31:33 +1000

    Remove unnecessary loop index increment

--- prac_05/birth_dates.py
+++ prac_05/birth_dates.py
@@ -5,7 +5,6 @@ name_DOB_dict = {}
 current_year = 2019
 index = 2
 for i in range(index):
-    i = i + 1
     name = input("Name: ")
     date_of_birth = input("Date of birth dd/mm/yyy: ")
     name_DOB_dict[date_of_birth] = name
