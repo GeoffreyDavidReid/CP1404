4718dd2c0200a8506c4214be1a2120c97fe03285
gdreid2015 <gdreid2015@hotmail.com>
2019-09-01 11:21:26 +1000

    Comment out max width function

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -17,3 +17,10 @@ print("Text: ", words_str)
 for key,val in word_count_dict.items():
     print("{}: {:>5d}".format(key, val))
 
+#def get_max_column_width(column_number, table):
+#   max_width = 0
+#    for row in table:
+#    current_width = len(row[column_number])
+#        if current_width > max_width
+#           max_width = current_width
+#    return max_width + 5
