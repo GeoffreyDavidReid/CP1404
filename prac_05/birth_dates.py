8769b6fa996b6fadb89cf73fdd20b1879f7637e4
gdreid2015 <gdreid2015@hotmail.com>
2019-09-01 16:04:31 +1000

    Create DOB dictionary

--- prac_05/birth_dates.py
+++ prac_05/birth_dates.py
@@ -7,12 +7,11 @@ index = 2
 for i in range(index):
     name = input("Name: ")
     date_of_birth = input("Date of birth dd/mm/yyy: ")
-    name_DOB_dict[date_of_birth] = name
     day, month, year = date_of_birth.split("/")
     age = current_year - int(year)
-    #print("Age: ", age)
+    name_DOB_dict[date_of_birth] = name, age
     print("Name: ", name ,"Date of Birth: ", date_of_birth , "Age: ", age)
 
-#print("Name DOB Dictionary: ", name_DOB_dict)
+print("Name DOB Dictionary: ", name_DOB_dict)
 
 
