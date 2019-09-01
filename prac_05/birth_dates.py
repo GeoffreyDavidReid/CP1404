5b022540d5eeaaefd959e7c090fed0267e0c7965
gdreid2015 <gdreid2015@hotmail.com>
2019-09-01 15:23:43 +1000

    Calculate each persons age and print it

--- prac_05/birth_dates.py
+++ prac_05/birth_dates.py
@@ -2,11 +2,15 @@
 
 name_DOB_dict = {}
 
+current_year = 2019
 index = 2
 for i in range(index):
     i = i + 1
     name = input("Name: ")
     date_of_birth = input("Date of birth dd/mm/yyy: ")
     name_DOB_dict[date_of_birth] = name
+    day, month, year = date_of_birth.split("/")
+    age = current_year - int(year)
+    print("Age: ", age)
 
 print("Name DOB Dictionary: ", name_DOB_dict)
