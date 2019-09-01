6335e762e01cfab71fa4cce57f44a8624b7fb47e
gdreid2015 <gdreid2015@hotmail.com>
2019-09-01 15:10:26 +1000

    Add code to create dictionary

--- prac_05/birth_dates.py
+++ prac_05/birth_dates.py
@@ -7,6 +7,6 @@ for i in range(index):
     i = i + 1
     name = input("Name: ")
     date_of_birth = input("Date of birth dd/mm/yyy: ")
-    name_DOB_dict = {name, date_of_birth}
+    name_DOB_dict[date_of_birth] = name
 
 print("Name DOB Dictionary: ", name_DOB_dict)
