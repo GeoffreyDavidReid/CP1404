7d699a8c8886a6d3466f099ff4bf3ddc28ff76c9
gdreid2015 <gdreid2015@hotmail.com>
2019-09-12 11:20:03 +1000

    Testing

--- prac_06/used_cars_solution.py
+++ prac_06/used_cars_solution.py
@@ -6,25 +6,20 @@ from car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("My car", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
     print(my_car)
-    my_car.add_fuel(20) #add this
-    my_car.odometer = 10 #add this
 
     print("Car {}, {}".format(my_car.fuel, my_car.odometer))
     print("Car {self.fuel}, {self.odometer}".format(self=my_car))
 
     limo = Car("Limo", 100)
-    print(limo.name) # add print
     limo.add_fuel(20)
     print(limo.fuel)
     limo.drive(115)
     print(limo.odometer)
 
-    Chevrolet = Car("Chevrolet", 100) #add new instance
-    print(Chevrolet.name) # add print
 
 main()
