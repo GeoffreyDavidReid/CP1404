d93bcef5f7789d71de1607c90562e4324919aced
gdreid2015 <gdreid2015@hotmail.com>
2019-09-13 18:41:47 +1000

    Added most code for my own guitar main program

---
+++ prac_06/myguitars.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 Practical
+Guitar program.
+"""
+from guitar import Guitar
+
+def main():
+    """Guitar program, using Guitar class."""
+    guitars = []
+    print("My guitars!")
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar_to_add = Guitar(name, year, cost)
+        guitars.append(guitar_to_add)
+        print(guitar_to_add,"added.")
