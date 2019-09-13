ba3c9216ab5e178d15219875ac43b3cc5ade36e0
gdreid2015 <gdreid2015@hotmail.com>
2019-09-13 17:37:00 +1000

    Added print statements to test aspects of myguitar Guitar class

--- prac_06/test_myguitar.py
+++ prac_06/test_myguitar.py
@@ -1,5 +1,26 @@
-#
-# test myguitar
-#
+"""
+CP1404/CP5632 Practical - Suggested Solution
+Basic manual tests for Guitar class
+"""
+from myguitar import Guitar # test this code using my guitar class
 
-from myguitar import Guitar # remove prac06 as the class name is guitar
+CURRENT_YEAR = 2019
+
+
+def run_tests():
+    """Tests for Guitar class."""
+    name = "Fender"
+    year = 1966
+    cost = 3600
+
+    guitar = Guitar(name, year, cost)
+    other = Guitar("Another guitar", 1954, 5000)
+
+    #print(guitar)
+
+    print("{} get_age() - Expected {}. Got {}".format(guitar.name,53,guitar.get_age()))
+    print("{} get_age() - Expected {}. Got {}".format(other.name, 65, other.get_age()))
+
+
+if __name__ == '__main__':
+    run_tests()
