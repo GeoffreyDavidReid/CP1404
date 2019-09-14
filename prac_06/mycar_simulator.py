e2d59900bd69032a3cf44f00a62c83a5a7becde2
gdreid2015 <gdreid2015@hotmail.com>
2019-09-14 15:39:02 +1000

    Adding print statements and menu code

--- prac_06/mycar_simulator.py
+++ prac_06/mycar_simulator.py
@@ -15,12 +15,24 @@ def main():
     car = Car(name, 100)
     print(car)
     #MENU
-    #choice = input("Enter your choice: ").lower()
+    choice = input("Enter your choice: ").lower()
+    while choice != "q":
+        if choice == "d":
+            distance_to_drive = int(input("How many km do you want to drive? "))
+            while distance_to_drive < 0:
+                print("Distance must be greater than zero !")
+                distance_to_drive = int(input("How many km do you want to drive? "))
+            distance_driven = car.drive(distance_to_drive)
+            print("The car drove {} km".format(distance_driven))
+
+
     distance_to_drive = 100
     distance_driven = car.drive(distance_to_drive)
     print("TEST Distance driven", distance_driven)
     print("TEST ", car)
-
+    fuel_to_add = 200
+    car.add_fuel(fuel_to_add)
+    print("TEST ", car)
 
 main()
 
