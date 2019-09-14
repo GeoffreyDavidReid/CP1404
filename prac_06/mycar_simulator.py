7b2c1bd61f707ce4260ed1fc8dc5b9ceb41e87cc
gdreid2015 <gdreid2015@hotmail.com>
2019-09-14 15:01:07 +1000

    Add code to test that drive works - if you drive 100 the odo goes from 0 to 100 and also fuel is used

--- prac_06/mycar_simulator.py
+++ prac_06/mycar_simulator.py
@@ -13,4 +13,14 @@ def main():
     print("Let's go")
     name = input("Enter your car name: ")
     car = Car(name, 100)
+    print(car)
+    #MENU
+    #choice = input("Enter your choice: ").lower()
+    distance_to_drive = 100
+    distance_driven = car.drive(distance_to_drive)
+    print("TEST Distance driven", distance_driven)
+    print("TEST ", car)
+
+
+main()
 
