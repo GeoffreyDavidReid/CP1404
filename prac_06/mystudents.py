23c7f0cae08b9d107d792a92c313ed7c4a917f27
gdreid2015 <gdreid2015@hotmail.com>
2019-09-14 18:03:23 +1000

    Create student main program to test Student class

---
+++ prac_06/mystudents.py
@@ -0,0 +1,11 @@
+""" My student main program
+    Test student class.
+"""
+from mystudent import Student #remove prac06 as the class name is guitar
+
+def main():
+
+    Student()
+    print(Student)
+
+main()
