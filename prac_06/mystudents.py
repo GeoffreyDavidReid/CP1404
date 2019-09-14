056ba04b3469930d2136572ac1b93d109744e290
gdreid2015 <gdreid2015@hotmail.com>
2019-09-14 19:01:03 +1000

    Testing change ID and change grade methods

--- prac_06/mystudents.py
+++ prac_06/mystudents.py
@@ -1,11 +1,21 @@
 """ My student main program
     Test student class.
 """
-from mystudent import Student #remove prac06 as the class name is guitar
+from mystudent import Student
 
 def main():
 
-    Student()
-    print(Student)
+    student = Student()
+    #print(student)
+    student.name = "Geoff"
+    student.ID = 123456
+    student.grade = "D"
+    #print(student)
+    new_student_grade = "HD"
+    #print("TEST ",new_student_grade)
+    student.changeGrade(new_student_grade)
+    new_student_ID = 111111
+    student.changeID(new_student_ID)
+    print(student)
 
 main()
