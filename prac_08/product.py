7f33669672f05bf277bcf0ae1dc62e5a84666d97
GeoffreyDavidReid <geoffrey.reid@my.jcu.edu.au>
2019-10-04 19:27:56 +1000

    Finish product init function

--- prac_08/product.py
+++ prac_08/product.py
@@ -4,4 +4,7 @@
 class Product:
     def __init__(self, name="", price=0.0, is_on_sale=False):
         print("Running init")
+        self.name = name
+        self.price = price
+        self.is_on_sale = is_on_sale
 
