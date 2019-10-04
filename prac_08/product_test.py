f8b8230f2c50ef46d1d7d9081e72929e593fcc59
GeoffreyDavidReid <geoffrey.reid@my.jcu.edu.au>
2019-10-04 19:45:47 +1000

    Refactor

---
+++ prac_08/product_test.py
@@ -0,0 +1,13 @@
+"""
+Testing my Product class
+"""
+
+from product import Product
+
+# name = input("Name: ")
+# price = input("Price: ")
+# is_on_sale = input("Is_on_sale: "))
+#
+# product1 = Product(name, price, is_on_sale)
+#
+# print(student1)
