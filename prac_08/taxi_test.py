c46f936907992daacff160b74499c3dcf532b2d4
GeoffreyDavidReid <geoffrey.reid@my.jcu.edu.au>
2019-10-04 15:07:37 +1000

    Test new odometer attribute

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -4,10 +4,10 @@ from car import Car
 
 name = input("Name: ")
 fuel = int(input("Fuel: "))
-#odometer = int(input("Odometer: "))
+odometer = int(input("Odometer: "))
 
-car1 = Car(name, fuel)
+car1 = Car(name, fuel, odometer)
 
-taxi = Car(name, fuel)
+taxi = Car(name, fuel, odometer)
 
 print(taxi)
