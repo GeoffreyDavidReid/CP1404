b237ef46ba2eee4e8dd80be21f714c453185d1e6
GeoffreyDavidReid <geoffrey.reid@my.jcu.edu.au>
2019-10-05 13:40:05 +1000

    Test Product class

--- prac_08/product_test.py
+++ prac_08/product_test.py
@@ -4,6 +4,10 @@ Testing my Product class
 
 from product import Product
 
+product1 = Product()
+
+print("Test: ",product1)
+
 # name = input("Name: ")
 # price = input("Price: ")
 # is_on_sale = input("Is_on_sale: "))
