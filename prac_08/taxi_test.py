84426336db21b680859204ba06403f3ec867c0bd
GeoffreyDavidReid <geoffrey.reid@my.jcu.edu.au>
2019-10-05 19:49:24 +1000

    Refactor

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -2,8 +2,14 @@
 
 from taxi import Taxi
 
-name = input("Name: ")
-fuel = int(input("Fuel: "))
-price_per_km = int(input("Price per km: "))
+def main():
 
-taxi1 = Taxi(name, fuel, price_per_km)
+    name = input("Name: ")
+    fuel = int(input("Fuel: "))
+    price_per_km = int(input("Price per km: "))
+
+    taxi1 = Taxi(name, fuel, price_per_km)
+    taxi1.drive(20)
+    print("TEST2", taxi1)
+
+main()
