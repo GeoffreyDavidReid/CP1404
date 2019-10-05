489a8387741142612cd1de55e4cb3ca9753b0888
GeoffreyDavidReid <geoffrey.reid@my.jcu.edu.au>
2019-10-06 08:26:56 +1000

    get fare

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -6,12 +6,12 @@ def main():
 
     name = input("Name: ")
     fuel = int(input("Fuel: "))
-    price_per_km = float(input("Price per km: "))
+    price_per_km = float(input("Price per km $: "))
 
     taxi1 = Taxi(name, fuel, price_per_km)
     taxi1.drive(40)
     print("TEST2", taxi1)
-    current_fare = taxi1.current_fare_distance * taxi1.price_per_km
+    current_fare = taxi1.get_fare()
     print("TEST3", current_fare)
 
 main()
