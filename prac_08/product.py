e9420b7224bb5d22e3371cf966686625a44b42a2
GeoffreyDavidReid <geoffrey.reid@my.jcu.edu.au>
2019-10-05 14:11:54 +1000

    Add _str_ function

--- prac_08/product.py
+++ prac_08/product.py
@@ -3,8 +3,10 @@
 """
 class Product:
     def __init__(self, name="", price=0.0, is_on_sale=False):
-        print("Running init")
         self.name = name
         self.price = price
         self.is_on_sale = is_on_sale
+
+    def __str__(self):
+        return("Name: {} Price: {} Is_on_sale: {}".format(self.name, self.price, self.is_on_sale))
 
